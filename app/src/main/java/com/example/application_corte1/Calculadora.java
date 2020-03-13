7fca0ce5e406d4ad938ef5c330d7894cf37f688e
fabian <facebian@gmail.com>
2020-03-13 00:25:00 -0500

    Se agregaron dos actividades mas: Calculadora y calificación

--- app/src/main/java/com/example/application_corte1/Calculadora.java
+++ app/src/main/java/com/example/application_corte1/Calculadora.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class BisiestoActivity extends AppCompatActivity {
+public class Calculadora extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bisiesto);
+        setContentView(R.layout.activity_calculadora);
     }
 
     public void volver (View v){
--- app/src/main/java/com/example/application_corte1/CalificacionActivity.java
+++ app/src/main/java/com/example/application_corte1/CalificacionActivity.java
@@ -6,17 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class BisiestoActivity extends AppCompatActivity {
+public class CalificacionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bisiesto);
+        setContentView(R.layout.activity_calificacion);
     }
 
     public void volver (View v){
         Intent i = new Intent(this,MainActivity.class);
         startActivity(i);
-
     }
 }
